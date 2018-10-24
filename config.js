0398cef34eefe3660604ee7304c2f3859ee360a9
cassianasl <cassianasilveira64@gmail.com>
2018-10-24 08:38:23 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export const config = {
+    apiKey: 'AIzaSyDBX3nYxP997EZgVULeHqv6LXUfEHhFezk',
+    authDomain: 'cobaia-ad99b.firebaseapp.com',
+    databaseURL: 'https://cobaia-ad99b.firebaseio.com',
+    projectId: 'cobaia-ad99b',
+    storageBucket: 'cobaia-ad99b.appspot.com',
+    messagingSenderId: '726486058008'
+};
